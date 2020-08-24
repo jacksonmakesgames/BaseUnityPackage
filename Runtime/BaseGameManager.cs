c9d2bd32fc26daa5c2e0920f156fa788743b5ade
Jackson <jacksonsdean@gmail.com>
2020-08-24 16:18:44 -0400

    Making dir structure

---
+++ Runtime/BaseGameManager.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Jackson
+{
+   
+    public class BaseGameManager : MonoBehaviour
+    {
+
+        public static bool playing = false;
+        public static int score;
+
+        [SerializeField]
+        protected GameObject _mainMenu;
+        protected static GameObject mainMenu = _mainMenu;
+
+        [SerializeField]
+        protected GameObject _playerPrefab;
+        protected static GameObject playerPrefab = _playerPrefab;
+
+        [SerializeField]
+        protected Transform playerSpawn;
+
+        
+
+        [SerializeField]
+        GameObject losePanel;
+
+        // Start is called before the first frame update
+        protected void Awake(){
+            _mainMenu = MainMenu;
+            ShowMainMenu();
+            SpawnPlayer();
+
+        }
+
+        public virtual static int GetScore(){
+            return score;
+        }
+
+        protected static virtual void ShowMainMenu(){
+            _mainMenu.SetActive(true);
+            if (mainMenu.GetComponent<Animator>()){
+                mainMenu.GetComponent<Animator>().SetTrigger("Show");
+            }
+        }
+
+
+        public virtual static void CloseMainMenu()
+        {
+            _mainMenu.SetActive(false);
+            if (mainMenu.GetComponent<Animator>())
+            {
+                mainMenu.GetComponent<Animator>().SetTrigger("Hide");
+            }
+        }
+        public virtual static void Lose(){
+            playing = false;
+            if (loseMenu){
+                losePanel.SetActive(true);
+                if (losePanel.GetComponent<Animator>()) {
+                    losePanel.GetComponent<Animator>().SetTrigger("Show");
+                }
+            }
+        }
+
+        protected virtual static void SpawnPlayer(){
+            Instantiate(playerPrefab, playerSpawn.position, playerSpawn.rotation, null);
+        }
+
+        public virtual static void ResetGame(){
+            if (losePanel.GetComponent<Animator>()){
+                losePanel.GetComponent<Animator>().SetTrigger("Hide");
+            }
+            SpawnPlayer();
+            StartGame();
+        }
+
+        public static virtual void StartGame(){
+            _mainMenu.SetActive(false);
+            ResetScore();
+            playing = true;
+            scoreAnim.gameObject.SetActive(true);
+            phaseManager.Restart();
+        }
+
+     
+
+        public static virtual void AddScore(int amt){
+            score += amt;
+            UpdateScoreText();
+        }
+
+
+
+        protected virtual static void ResetScore(){
+            score = 0;
+            UpdateScoreText();
+        }
+
+        protected virtual static void UpdateScoreText()
+        {
+            if (scoreText)
+                scoreText.text = score.ToString();
+        }
+       
+
+    }
+}
---
+++ Runtime/Utilities.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Jackson
+{
+
+    public struct ScreenEdges
+    {
+        public float maxY;
+        public float minY;
+        public float maxX;
+        public float minX;
+    }
+
+    public class Utilities
+    {
+        public static ScreenEdges screenEdges;
+
+
+
+        static void UpdateScreenEdges()
+        {
+            screenEdges = new ScreenEdges();
+
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(.5f, 1.0f, 0));
+            if (Physics.Raycast(ray, out hit))
+                screenEdges.maxY = hit.point.y - hit.point.y / 4;
+
+            ray = Camera.main.ViewportPointToRay(new Vector3(.5f, 0f, 0));
+            if (Physics.Raycast(ray, out hit))
+                screenEdges.minY = hit.point.y - hit.point.y / 4;
+
+            ray = Camera.main.ViewportPointToRay(new Vector3(0.0f, .5f, 0));
+            if (Physics.Raycast(ray, out hit))
+                screenEdges.minX = hit.point.x - hit.point.x / 4;
+
+            ray = Camera.main.ViewportPointToRay(new Vector3(1.0f, .5f, 0));
+            if (Physics.Raycast(ray, out hit))
+                screenEdges.maxX = hit.point.x - hit.point.x / 4;
+
+
+            screenEdges.maxX = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+            screenEdges.minX = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+            screenEdges.maxY = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+            screenEdges.minY = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+
+        }
+
+    }
+}
